bde777a29c862955227281a3ea1b41a2f77d4a04
hnrqi <hnrqi@icloud.com>
2022-12-04 17:18:52 -0300

    Final

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.*;
 
 import java.time.LocalDate;
 
@@ -20,9 +19,47 @@ public class Main {
         mentoria.setDescricao("descricao mentoria de java");
         mentoria.setData(LocalDate.now());
 
-
-        System.out.println(curso1);
+        /* System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria);
+        System.out.println(mentoria); */
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descricao Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devHenrique = new Dev();
+        devHenrique.setNome("Henrique");
+        devHenrique.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Henrique: " + devHenrique.getConteudosInscritos());
+
+        devHenrique.progredir();
+        devHenrique.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos Henrique: " + devHenrique.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos Henrique: " + devHenrique.getConteudosConcluidos());
+        System.out.println("XP: " + devHenrique.calcularTotalXP());
+
+        System.out.println("---");
+
+        Dev devIsabelly = new Dev();
+        devIsabelly.setNome("Isabelly");
+        devIsabelly.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Isabelly: " + devIsabelly.getConteudosInscritos());
+
+        devIsabelly.progredir();
+        devIsabelly.progredir();
+        devIsabelly.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos Isabelly: " + devIsabelly.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos Isabelly: " + devIsabelly.getConteudosConcluidos());
+        System.out.println("XP: " + devIsabelly.calcularTotalXP());
+
+
+
+
+
     }
 }
